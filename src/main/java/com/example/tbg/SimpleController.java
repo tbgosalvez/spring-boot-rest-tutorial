df951839f83789702875bb20eb8806fc0dff0086
tbgosalvez <tbgosalvez@gmail.com>
2020-03-11 18:52:53 -0400

    added homepage and its controller

---
+++ src/main/java/com/example/tbg/SimpleController.java
@@ -0,0 +1,15 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class SimpleController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
