7bdaf24e2c587a81bb20131c0c27fb5a5f20f2a7
tbgosalvez <tbgosalvez@gmail.com>
2020-03-11 18:11:50 -0400

    init

---
+++ src/main/java/com/example/tbg/TbgApplication.java
@@ -0,0 +1,13 @@
+package com.example.tbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tbg/TbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
